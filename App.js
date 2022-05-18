46d43141777a8261b043e9ee8c15637c83ad35e6
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-05-18 17:11:37 +0300

    second  commit

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+let openMenu = document.querySelector(".open-menu");
+let nav = document.querySelector("nav");
+
+openMenu.addEventListener ("click", () => {
+    nav.classList.toggle("active")
+});
+
+// Open & Close The Project Menu
+let popUup = document.querySelector(".pop-up");
+let down = document.querySelector(".item.down");
+
+down.addEventListener("click", () => {
+    popUup.classList.toggle("active");
+});
